0a4ac461802203c7421cab09dd1aaed3180e13fa
GeekAmita <amitasingh5661@gmail.com>
2023-06-27 11:06:53 +0530

    Added interest container in interests

--- src/components/interests/InterestBox.jsx
+++ src/components/interests/InterestBox.jsx
@@ -2,26 +2,30 @@ import "./InterestBox.css";
 import interestList from "../../data/InterestList";
 
 function InterestBox() {
-  return interestList.map((item, index) => {
-    return (
-      <div>
-        <div className="case">
-          <div className="iconBox">
-            <img className="icon" src={item.icon} alt={item.topic} />
+  return (
+    <div className="interestContainer">
+      {interestList.map((item, index) => {
+        return (
+          <div>
+            <div className="case">
+              <div className="iconBox">
+                <img className="icon" src={item.icon} alt={item.topic} />
+              </div>
+              <a
+                className="detailBox"
+                href={item.join}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="topic">{item.topic}</div>
+                <div className="detail">{item.detail}</div>
+              </a>
+            </div>
           </div>
-          <a
-            className="detailBox"
-            href={item.join}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="topic">{item.topic}</div>
-            <div className="detail">{item.detail}</div>
-          </a>
-        </div>
-      </div>
-    );
-  });
+        );
+      })}
+    </div>
+  );
 }
 
 export default InterestBox;
